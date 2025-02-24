92f55be1bbdaa68b05d9803bcba0cb40d35418ee
IK8933 <iankessack1989@gmail.com>
2025-02-24 14:09:59 -0500

    adding seeding to render build

--- client/vite.config.js
+++ client/vite.config.js
@@ -34,37 +34,3 @@ export default defineConfig({
     port: 3000,
   },
 });
-
-
-// export default defineConfig({
-//   plugins: [
-//     react(), // You're missing the react plugin
-//     tailwindcss(),
-//   ],
-//   server: {
-//     allowedHosts: [
-//       'mov-api-e-client.onrender.com', // Remove https:// from hosts
-//       'mov-api-e-server.onrender.com',
-//       '0.0.0.0'
-//     ],
-//     port: 3001,
-//     open: true,
-//     host: "0.0.0.0",
-//     proxy: {
-//       "/api": {
-//         target: "https://mov-api-e-server.onrender.com",
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//       "/auth": {
-//         target: "https://mov-api-e-server.onrender.com",
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//   },
-//   preview: {
-//     host: true,
-//     port: 3000,
-//   },
-// });
